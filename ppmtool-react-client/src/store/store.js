f81336afe677e85d591608b3d7bb7826ef9c553a
Jeonghoon-Lee <hoon22.lee@gmail.com>
2020-05-21 09:01:48 -0400

    fixed bug

--- ppmtool-react-client/src/store/store.js
+++ ppmtool-react-client/src/store/store.js
@@ -2,21 +2,17 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-// import * as actions from './actions'
-
-// const authInterceptor = ({ dispatch }) => (next) => (action) => {
-//   if (action.status === 401) {
-//     console.log('error.. logout')
-//     dispatch(actions.logout());
-//   } else {
-//     next(action);
-//   }
-// };
-
 const middleware = [thunk]
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)))
+console.log(composeEnhancers, process.env.NODE_ENV)
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleware))
+)
 
 // let store
 // if (window.navigator.userAgent.includes('Chrome')) {
